d1628994fd5a8134c3d6a829480b2797b3048b57
GuNShOtzZ <kevinloeding@outlook.de>
2024-02-23 04:24:50 +0100

    Assemble all components in Index.js

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,15 @@
+import About from "@/components/About/About";
+import Introduction from "@/components/Introduction/Introduction";
+import Projects from "@/components/Projects/Projects";
+import Skills from "@/components/Skills/Skills";
+
 export default function Home() {
   return (
     <>
-      <h1>Hello World!</h1>
+      <Introduction />
+      <About />
+      <Skills />
+      <Projects />
     </>
   );
 }
