416c6da881f10f7da4ac5559973ce511bf26360b
GuNShOtzZ <kevinloeding@outlook.de>
2024-02-26 20:44:43 +0100

    Optimize display of Header and Introduction for mobile devices

--- components/Header/Header.jsx
+++ components/Header/Header.jsx
@@ -2,7 +2,7 @@ import Socials from "./Socials";
 
 export default function Header() {
   return (
-    <header className=" bg-dark-300 flex justify-between items-center p-6">
+    <header className=" bg-dark-300 sticky top-0 shadow-lg flex flex-col tablet:flex-row tablet:justify-between gap-4 items-center p-6">
       <h1 className=" text-2xl">Kevin Loeding</h1>
       <Socials />
     </header>
--- components/Introduction/Introduction.jsx
+++ components/Introduction/Introduction.jsx
@@ -4,12 +4,12 @@ import IntroductionText from "./IntroductionText";
 
 export default function Introduction() {
   return (
-    <div className="flex px-20 py-8 justify-between items-center">
+    <div className="flex desktop:px-[25vw] px-8 py-8 justify-between gap-8 items-center">
       <div className="flex flex-col gap-4">
         <IntroductionText />
         <ContactButton />
       </div>
-      <div className=" bg-dark-300 px-4 pt-4 rounded">
+      <div className=" bg-dark-300 hidden tablet:block px-4 pt-4 shadow-lg rounded">
         <Image src={"/avatar.png"} alt="Avatar" width={248} height={248} />
       </div>
     </div>
