6e8c5de82717799bdab58f35c198ca694ae45d1f
GuNShOtzZ <kevinloeding@outlook.de>
2024-02-26 03:46:50 +0100

    Display last commit from main branch in footer

--- components/Footer/Footer.jsx
+++ components/Footer/Footer.jsx
@@ -1,8 +1,31 @@
+import axios from "axios";
+import Link from "next/link";
+import { useState, useEffect } from "react";
+
 export default function Footer() {
+  const [commitHash, setCommitHash] = useState("");
+
+  useEffect(() => {
+    const fetchLastCommitHash = async () => {
+      try {
+        const response = await axios.get(
+          "https://api.github.com/repos/kevinldg/personal-website/commits/main"
+        );
+        setCommitHash(response.data.sha.slice(0, 7));
+      } catch (error) {
+        console.error("Fehler beim Abrufen des Commits:", error);
+      }
+    };
+
+    fetchLastCommitHash();
+  }, []);
+
   return (
     <footer className="p-8 flex justify-between items-center bg-vsc-blue">
       <p>© 2024 Kevin Loeding</p>
-      <p>a26bf1</p>
+      <Link href="https://github.com/kevinldg/personal-website/commits/main/">
+        Last Commit: {commitHash}
+      </Link>
     </footer>
   );
 }
